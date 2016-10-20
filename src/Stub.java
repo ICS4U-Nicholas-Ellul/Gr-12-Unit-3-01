20a0bbe0fc279de580c09a5e8ba6e63b147a775f
Nicholas Ellul <nicholas.ellul1@gmail.com>
2016-10-20 12:41:41 -0400

    Finished using recursion.

--- src/Stub.java
+++ src/Stub.java
@@ -1,20 +1,41 @@
+/****************************************************************************
+ *
+ * Created by: Nicholas Ellul
+ * Created on: Oct 2016
+ * This program uses recursion to reverse an array.
+ *    
+ ****************************************************************************/
+
+import java.util.Scanner;
 
 public class Stub {
 
-	public void ReverseString(String sentence){
+	public static void ReverseString(String sentence){
 		
 		String sentenceSubString;
-		char lastCharacter;
+		String lastCharacter;
+		String reversedText = "";
 		
 		if(sentence.length() > 0){
-			//sentenceSubString 
+			
+			//gets part of string
+			sentenceSubString = sentence.substring(0, sentence.length() - 1);
+			int bob = sentence.length();
+			ReverseString(sentenceSubString);
+			
+			//outputs it
+			lastCharacter = sentence.substring(sentence.length() - 1);
+			reversedText = lastCharacter + reversedText;
+			System.out.println(reversedText);
 		}
 		
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner reader = new Scanner(System.in);
+		
+		ReverseString(reader.nextLine());
 	}
 
 }
